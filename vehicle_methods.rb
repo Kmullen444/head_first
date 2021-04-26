6f2163f3314b5a12f92e8679f2557913f4e461c5
kmullen444 <kmullen444@gmail.com>
2021-04-26 15:43:51 -0700

    "Added method for MPG of a vehicle"

--- vehicle_methods.rb
+++ vehicle_methods.rb
@@ -13,6 +13,20 @@ def use_headlights(brightness = "low-beams")
   puts "Watch out for the deer!"
 end
 
+
+def mileage(miles_driven, gas_used)
+  if gas_used == 0
+    return 0.0
+  end
+  miles_driven/gas_used
+end
+
+trip_mileage = mileage(400,12)
+puts "You got #{trip_mileage} MPG on this trip."
+
+lifetime_mileage = mileage(11432,366)
+puts "This car averages #{lifetime_mileage} MPG."
+
 sound_horn
 accelerate
 use_headlights("brights")
