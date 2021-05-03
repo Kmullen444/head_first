a3b8e902e7cbd01228be2cebb6c3e61d8309e1cd
kmullen444 <kmullen444@gmail.com>
2021-05-03 13:40:50 -0700

    Added logic to validate data from the user

--- storybook.rb
+++ storybook.rb
@@ -1,38 +1,60 @@
 class Bird
-  def talk 
-    puts "Chirp! Chirp"
+  def talk (name) 
+    puts "#{name} says Chirp! Chirp"
   end
 
-  def move(destination)
-    puts "Flying to the #{destination}."
+  def move(name, destination)
+    puts "#{name} flies to the #{destination}."
   end
 end
 
 class Dog
+  attr_reader :name, :age
+
+  def name=(value)
+    if value == ""
+      raise "Name can't be blank!"
+    end
+    @name = value
+  end
+
+  def age=(value)
+    if value < 0
+      raise "An age of #{value} isn't valid!"
+    end
+    @age = value
+  end
+      
+  def report_age
+    puts "#{@name} is #{@age} years old."
+  end
+
   def talk
-    puts "Bark!"
+    puts "#{@name} says Bark!"
   end
 
   def move(destination)
-    puts "Running to the #{destination}."
+    puts "#{@name} runs to the #{destination}."
   end
 end
 
 class Cat
-  def talk 
-    puts "Meow!"
+  def talk (name) 
+    puts "#{name} says Meow!"
   end
 
-  def move(destination)
-    puts "Running to the #{destination}."
+  def move(name, destination)
+    puts "#{name} runs to the #{destination}."
   end
 end
 
-bird = Bird.new
+
 dog = Dog.new
+dog_name = "Lucy"
+dog.talk(dog_name)
+dog.move(dog_name, "fence")
+
 cat = Cat.new
- 
-bird.move("tree")
-dog.talk
-bird.talk
-cat.move("house")
+cat_name = "Fluffly"
+cat.talk(cat_name)
+cat.move(cat_name, "litter box")
