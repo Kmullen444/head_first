f29c167e3b9f5d1ec689ae1ce47f99a3e68c0dd8
kmullen444 <kmullen444@gmail.com>
2021-05-13 18:55:52 -0700

    Added 2 subclasses and started refactoring superclass

--- employee.rb
+++ employee.rb
@@ -15,15 +15,70 @@ class Employee
     @salary = value
   end
 
+  def initialize(name = "Anonymous", salary = 0.0)
+    self.name = name
+    self.salary = salary
+  end
+
   def print_pay_stub
-    puts "Name: #{@name}"
-    pay_for_period = (@salary / 365.0) * 14
+    puts "Name: #{name}"
+    pay_for_period = (salary / 365.0) * 14
     formatted_pay = format("%.2f", pay_for_period)
     puts "Pay for this period: $#{formatted_pay}."
   end
 end
 
-amy = Employee.new
-amy.name = "Amy Blake"
-amy.salary = 50000
-amy.print_pay_stub
+class SalariedEmployee < Employee
+  attr_reader :salary
+
+  def initialize(salary = 0.0)
+    self.salary = salary
+  end
+
+  def salary= (value)
+    if value < 0
+      raise "Salary can't be a negitive number"
+    end
+    @salary = value
+  end
+
+  def print_pay_stub
+    puts "Name: #{name}"
+    pay_for_period = (salary / 365.0) * 14
+    formatted_pay = format("%.2f", pay_for_period)
+    puts "Pay for this period: $#{formatted_pay}."
+  end
+end
+
+class HourlyEmployee < Employee
+  attr_reader :hourly_wage, :hours_per_week
+
+  def initialize(hourly_wage = 0.0, hours_per_week = 0.0)
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
+  end
+
+  def hourly_wage= (value)
+    if hourly_wage < 0
+      raise "Number can't be negitive"
+    end
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week= (value)
+    if hours_per_week < 0 
+      raise "Number can't be negitive"
+    end
+    @hours_per_week = hours_per_week
+  end
+
+  def print_pay_stub
+    puts "Name: #{name}"
+    pay_for_period = (hourly_wage * hours_per_week) * 14
+    formatted_pay = format("%.2f", pay_for_period)
+    puts "Pay for this period: $#{formatted_pay}."
+  end
+end
+
+
+
