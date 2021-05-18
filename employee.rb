9a8924598062be4323243f83d0c37cdc09b8efc1
kmullen444 <kmullen444@gmail.com>
2021-05-17 20:57:07 -0700

    Refactored code for better use of inheritance. Created factory methods for hourly class.

--- employee.rb
+++ employee.rb
@@ -1,5 +1,5 @@
 class Employee
-  attr_reader :name, :salary
+  attr_reader :name
 
   def name=(value)
     if value == ""
@@ -7,31 +7,21 @@ class Employee
     end
     @name = value
   end
-
-  def salary= (value)
-    if value < 0
-      raise "That is a negivtive number"
-    end
-    @salary = value
-  end
-
-  def initialize(name = "Anonymous", salary = 0.0)
-    self.name = name
-    self.salary = salary
-  end
-
-  def print_pay_stub
+  
+  def print_name
     puts "Name: #{name}"
-    pay_for_period = (salary / 365.0) * 14
-    formatted_pay = format("%.2f", pay_for_period)
-    puts "Pay for this period: $#{formatted_pay}."
+  end
+
+  def initialize(name = "Anonymous")
+    self.name = name
   end
 end
 
 class SalariedEmployee < Employee
   attr_reader :salary
 
-  def initialize(salary = 0.0)
+  def initialize(name = "Anonymous", salary = 0.0)
+    super(name)
     self.salary = salary
   end
 
@@ -42,8 +32,8 @@ class SalariedEmployee < Employee
     @salary = value
   end
 
-  def print_pay_stub
-    puts "Name: #{name}"
+   def print_pay_stub
+    print_name
     pay_for_period = (salary / 365.0) * 14
     formatted_pay = format("%.2f", pay_for_period)
     puts "Pay for this period: $#{formatted_pay}."
@@ -53,28 +43,41 @@ end
 class HourlyEmployee < Employee
   attr_reader :hourly_wage, :hours_per_week
 
-  def initialize(hourly_wage = 0.0, hours_per_week = 0.0)
+  def initialize(name = "Anonymous",hourly_wage = 0.0, hours_per_week = 0.0)
+    super(name)  
     self.hourly_wage = hourly_wage
     self.hours_per_week = hours_per_week
   end
 
+  def self.security_guard(name)
+    HourlyEmployee.new(name, 19.25, 30)
+  end
+
+  def self.cashier(name)
+    HourlyEmployee.new(name, 12.75, 25)
+  end
+
+  def self.janitor(name)
+    HourlyEmployee.new(name, 10.50, 20)
+  end
+
   def hourly_wage= (value)
-    if hourly_wage < 0
+    if value < 0
       raise "Number can't be negitive"
     end
-    @hourly_wage = hourly_wage
+    @hourly_wage = value
   end
 
   def hours_per_week= (value)
-    if hours_per_week < 0 
+    if value < 0 
       raise "Number can't be negitive"
     end
-    @hours_per_week = hours_per_week
+    @hours_per_week = value
   end
 
   def print_pay_stub
-    puts "Name: #{name}"
-    pay_for_period = (hourly_wage * hours_per_week) * 14
+    print_name
+    pay_for_period = hourly_wage * hours_per_week * 2
     formatted_pay = format("%.2f", pay_for_period)
     puts "Pay for this period: $#{formatted_pay}."
   end
@@ -82,3 +85,11 @@ end
 
 
 
+angela = HourlyEmployee.security_guard("Angela Matthews")
+edwin = HourlyEmployee.janitor("Edwin Burgess")
+ivan = HourlyEmployee.cashier("Ivan Stokes")
+
+
+angela.print_pay_stub
+edwin.print_pay_stub
+ivan.print_pay_stub
